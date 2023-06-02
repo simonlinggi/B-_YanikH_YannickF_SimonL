7636b40be172ae8266771a7a483f17db38f61174
yanikhuwiler <132079382+yanikhuwiler@users.noreply.github.com>
2023-06-02 16:47:53 +0200

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,44 @@
+// (1) Variablen initialisieren
+const formContainer = document.getElementById("formContainer");
+const gameContainer = document.getElementById("game-container");
+const submitButton = document.getElementById("submit");
+submitButton.disabled = true;
+const emailField = document.getElementById("email");
+
+// (2) Interaktionen festlegen
+emailField.addEventListener("keyup", () => {
+  onChangeEmailField();
+});
+submitButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+  onClickSubmit();
+});
+
+// (3) Interaktionen Code
+const onChangeEmailField = () => {
+  if (emailField.value === "") {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+};
+const onClickSubmit = async () => {
+  // Daten aus dem Formular für die Datenbank bereitstellen
+  const data = {
+    group: "teacher", // SQL Gruppen Namen
+    pw: "02bd77f9", // SQL Passwort
+    tableName: "user", // Name der Tabelle in der SQL Datenbank
+
+    columns: {
+      // "email" Name der Spalte in der SQL Tabelle
+      // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
+      email: emailField.value,
+    },
+  };
+  // Speichert die Daten in der Datenbank
+  await databaseClient.insertInto(data);
+
+  // Nach dem Speichern verschwindet das Formular, das Game erscheint
+  formContainer.classList.add("hidden");
+  gameContainer.classList.remove("hidden");
+};
