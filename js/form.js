da94ee0673d0419eb01cb263a5d7783ddb5544d7
YannickFink <132079520+YannickFink@users.noreply.github.com>
2023-06-07 22:52:27 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -3,12 +3,38 @@ const formContainer = document.getElementById("formContainer");
 const gameContainer = document.getElementById("game-container");
 const submitButton = document.getElementById("submit");
 submitButton.disabled = true;
+const nameField = document.getElementById("name");
 const emailField = document.getElementById("email");
+const phoneField = document.getElementById("phone");
+const adresseField = document.getElementById("adresse");
+const plzField = document.getElementById("plz");
+const answerField = document.getElementById("answer");
 
 // (2) Interaktionen festlegen
+nameField.addEventListener("keyup", () => {
+  onChangeNameField();
+});
+
+phoneField.addEventListener("keyup", () => {
+  onChangePhoneField();
+});
+
+adresseField.addEventListener("keyup", () => {
+  onChangeAdresseField();
+});
+
+plzField.addEventListener("keyup", () => {
+  onChangePLZField();
+});
+
+answerField.addEventListener("keyup", () => {
+  onChangeAnswerField();
+});
+
 emailField.addEventListener("keyup", () => {
   onChangeEmailField();
 });
+
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
   onClickSubmit();
